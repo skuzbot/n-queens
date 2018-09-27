756ee9abb6cc269095279a24c410e305e0c08355
Steven Duncan <steven.k.duncan@gmail.com>
2018-09-27 12:24:28 -0500

    finish bare minimum of sprint

--- src/solvers.js
+++ src/solvers.js
@@ -19,15 +19,15 @@
 // if n = even, only have to n/2 cards
 //if n is odd, Math.ceil(n/2) cards
 //for each solution. return mirrored solution as well.
-var makeMatrix = function(n) {
+var makeMatrix = function (n) {
   var matrix = [];
-  for(var x=0; x<n; x++){
+  for (var x = 0; x < n; x++) {
     var emptyRow = Array.apply(null, Array(n)).map(Number.prototype.valueOf, 0);
-    emptyRow.splice(x,1,1);
+    emptyRow.splice(x, 1, 1);
     matrix.push(emptyRow);
   }
   return matrix;
-}
+};
 var makeEmptyMatrix = function (n) {
   return _(_.range(n)).map(function () {
     return _(_.range(n)).map(function () {
@@ -36,77 +36,100 @@ var makeEmptyMatrix = function (n) {
   });
 };
 
-var permutate = function(array) { //given an array
+var permutate = function (array) { //given an array
   var result = []; //final array of arrays to be returned as all possible permutations
-  function perm(array, accumulator){ // recursive fuction takes a unique array, and 
+  perm = function(array, accumulator) { // recursive fuction takes a unique array, and 
     var currentArray;
     accumulator = accumulator || [];
-    for(var x=0; x<array.length; x++){
-      currentArray = array.splice(x,1);
-      if(array.length === 0){
+    for (var x = 0; x < array.length; x++) {
+      currentArray = array.splice(x, 1);
+      if (array.length === 0) {
         result.push(accumulator.concat(currentArray));
       }
-      perm(array.slice(), accumulator.concat(currentArray))
-      array.splice(x, 0, currentArray[0])
+      perm(array.slice(), accumulator.concat(currentArray));
+      array.splice(x, 0, currentArray[0]);
     }
     return result;
-  }
+  };
   return perm(array);
-}
+};
 
-window.findNRooksSolution = function(n) {
+window.findNRooksSolution = function (n) {
 
-  var solution = makeMatrix(n) //fixme
+  var solution = makeMatrix(n); //fixme
 
   console.log('Single solution for ' + n + ' rooks:', JSON.stringify(solution));
   return solution;
 };
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
-window.countNRooksSolutions = function(n) {
+window.countNRooksSolutions = function (n) {
   var matrix = makeMatrix(n);
-  var arr = permutate(matrix)
-  var solutionCount = arr.length; 
+  var arr = permutate(matrix);
+  var solutionCount = arr.length;
 
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
-window.findNQueensSolution = function(n) {
+window.findNQueensSolution = function (n) {
   var solution = 0;
-  if(n === 0){
-    return []
+  if (n === 0) {
+    return [];
   }
-  if(n===2||n===3){
+  if (n === 2 || n === 3) {
     return makeEmptyMatrix(n);
   }
 
   var matrix = makeMatrix(n);
   var arr = permutate(matrix);
-  for(var x=0; x<arr.length; x++){
-    var board = new Board(arr[x])
-    if(!board.hasAnyQueensConflicts()){
+  for (var x = 0; x < arr.length; x++) {
+    var board = new Board(arr[x]);
+    if (!board.hasAnyQueensConflicts()) {
       solution = arr[x];
     }
   }
-  
-  
-  
 
-  
-  
+
+
+
+
+
 
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
-window.countNQueensSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+window.countNQueensSolutions = function (n) {
+  var solutionCount = 0;
+  var matrix = makeMatrix(n);
+  var permArrays = permutate(matrix);
+
+  var solution = 0;
+  if (n === 0) {
+    return 1;
+  }
+
+  if (n === 2 || n === 3) {
+    return 0;
+  }
+
+  for (let i = 0; i < permArrays.length; i++) {
+    var board = new Board(permArrays[i]);
+    if (!board.hasAnyQueensConflicts()) {
+      solutionCount++;
+    }
+  }
+
+
+
+
+
 
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
 };
 
-console.log(permutate(array1))
+console.log(permutate(array1));
